6d25574629d7d72ac5e3e089c1b5568d112e0496
marshi <mashi3648@gmail.com>
2020-03-08 00:26:57 +0900

    addition, and remove.

--- app/src/main/java/marshi/android/uicomponent/Adapter.kt
+++ app/src/main/java/marshi/android/uicomponent/Adapter.kt
@@ -11,7 +11,7 @@ import java.lang.IllegalArgumentException
 class Adapter(val list: MutableList<Item>) : RecyclerView.Adapter<VH>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = when(viewType) {
+        val binding = when (viewType) {
             0 -> NormalItemBinding.inflate(inflater, parent, false)
             1 -> ExpandItemBinding.inflate(inflater, parent, false)
             else -> throw IllegalArgumentException()
@@ -24,30 +24,52 @@ class Adapter(val list: MutableList<Item>) : RecyclerView.Adapter<VH>() {
         notifyItemInserted(list.size - 1)
     }
 
+    private fun add(item: Item, position: Int) {
+        list.add(position, item)
+        notifyItemInserted(position)
+    }
+
+    private fun remove(item: Item, position: Int) {
+        list.remove(item)
+        notifyItemRemoved(position)
+    }
+
     override fun getItemCount() = list.size
 
     override fun onBindViewHolder(holder: VH, position: Int) {
         val itemView = holder.itemView
         val item = list[position]
+        if (item !is NormalItem) return
+        if (holder.binding is NormalItemBinding) {
+            holder.binding.text.text = item.text
+        }
         itemView.setOnClickListener {
+            val itemPosition = list.indexOf(item)
             if (itemView.elevation == 0f) {
                 itemView.elevation = 20f
             } else {
                 itemView.elevation = 0f
             }
-            (item as? NormalItem)?.isOpened = true
-            add(ExpandItem)
+            val normalItem = item as? NormalItem
+            if (normalItem?.isOpened == true) {
+                normalItem.isOpened = false
+                val expandItem = list[itemPosition + 1] as ExpandItem
+                remove(expandItem, itemPosition + 1)
+            } else {
+                normalItem?.isOpened = true
+                add(ExpandItem(itemPosition + 1), itemPosition + 1)
+            }
         }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return when(list[position]) {
+        return when (list[position]) {
             is NormalItem -> 0
             is ExpandItem -> 1
         }
     }
 }
 
-class VH(itemView: ViewDataBinding) : RecyclerView.ViewHolder(itemView.root) {
+class VH(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
 
 }
--- app/src/main/java/marshi/android/uicomponent/Item.kt
+++ app/src/main/java/marshi/android/uicomponent/Item.kt
@@ -2,7 +2,7 @@ package marshi.android.uicomponent
 
 sealed class Item
 
-object ExpandItem : Item()
+class ExpandItem(id: Int) : Item()
 
 class NormalItem(val text: String = "text", var isOpened: Boolean = false) : Item() {
 }
